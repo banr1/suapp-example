77cc45cefded45f0e0a94a80a43a8a9874d5ab28
banri <banri.yanahama@gmail.com>
2024-04-08 13:43:15 +0900

    add event in offchain function

--- src/MyFirstSuApp.sol
+++ src/MyFirstSuApp.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.8;
 
 contract MyFirstSuapp { 
+    event OffchainEvent(uint256 num);
+
     function onchain() public {}
 
-    function offchain() public pure returns (bytes memory) {
-        /* This is where you will write all your compute-heavy,
-        off-chain logic to be done in a Kettle */
+    function offchain() public returns (bytes memory) {
+        emit OffchainEvent(1);
+        emit OffchainEvent(2);
+
         return abi.encodeWithSelector(this.onchain.selector);
     }
 }
