3c7d553d9054d41539c0b8f5985b5e471ba03f6f
banri <banri.yanahama@gmail.com>
2024-04-11 18:12:06 +0900

    add draft of sending signed tx to ethereum testnet goerli

--- contracts/src/ConfidentialStore.sol
+++ contracts/src/ConfidentialStore.sol
@@ -5,12 +5,14 @@ import "suave-std/Suapp.sol";
 import "suave-std/Context.sol";
 import "suave-std/Transactions.sol";
 import "suave-std/suavelib/Suave.sol";
+import "solady/src/utils/LibString.sol";
 
 contract ConfidentialStore is Suapp {
     Suave.DataId signingKeyRecord;
-    string public PRIVATE_KEY = "KEY";
-
-    event TxnSignature(bytes32 r, bytes32 s);
+    string public constant PRIVATE_KEY = "KEY";
+    uint public constant GOERLI_CHAINID = 5;
+    string public constant GOERLI_CHAINID_STR = "0x5";
+    string public constant INFURA_GOERLI_RPC = "https://goerli.infura.io/v3/1301ac078b854c40887bdc6d21d2e2da";
 
     function registerPrivateKeyOnchain(Suave.DataId _signingKeyRecord) public {
         signingKeyRecord = _signingKeyRecord;
@@ -31,20 +33,34 @@ contract ConfidentialStore is Suapp {
     function sendTxOnchain() public emitOffchainLogs {}
 
     function sendTx() public returns (bytes memory) {
-        bytes memory signingKey = Suave.confidentialRetrieve(signingKeyRecord, PRIVATE_KEY);
-
-        Transactions.EIP155Request memory txnWithToAddress = Transactions.EIP155Request({
+        Transactions.EIP155Request memory txn = Transactions.EIP155Request({
             to: address(0x00000000000000000000000000000000DeaDBeef),
             gas: 1000000,
             gasPrice: 500,
-            value: 1,
+            value: 0,
             nonce: 1,
             data: bytes(""),
-            chainId: 1337
+            chainId: GOERLI_CHAINID
         });
 
-        Transactions.EIP155 memory txn = Transactions.signTxn(txnWithToAddress, string(signingKey));
-        emit TxnSignature(txn.r, txn.s);
+        bytes memory txRlp = Transactions.encodeRLP(txn);
+        bytes memory signingKey = Suave.confidentialRetrieve(signingKeyRecord, PRIVATE_KEY);
+
+        bytes memory txSigned = Suave.signEthTransaction(txRlp, GOERLI_CHAINID_STR, string(signingKey));
+        bytes memory body = abi.encodePacked(
+            '{"jsonrpc":"2.0","method":"eth_sendRawTransaction","params":["', 
+            LibString.toHexString(txSigned), 
+            '"],"id":1}'
+        );
+        /* solhint-enable */
+        Suave.HttpRequest memory request;
+        request.method = "POST";
+        request.body = body;
+        request.headers = new string[](1);
+        request.headers[0] = "Content-Type: application/json";
+        request.withFlashbotsSignature = false;
+        request.url = INFURA_GOERLI_RPC;
+        Suave.doHTTPRequest(request);
 
         return abi.encodeWithSelector(this.sendTxOnchain.selector);
     }
